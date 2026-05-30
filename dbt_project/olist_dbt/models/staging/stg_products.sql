8c0abe9f6049dc12a527342b5f4d459168c2fdc4
pqkzzz <0901297304a@gmail.com>
2026-05-30 13:06:24 +0700

    chore: cast data type

--- dbt_project/olist_dbt/models/staging/stg_products.sql
+++ dbt_project/olist_dbt/models/staging/stg_products.sql
@@ -7,13 +7,13 @@ SELECT
         'unknown'
     ) AS product_category_name,
 
-    p.product_name_lenght AS product_name_length,
-    p.product_description_lenght AS product_description_length,
-    p.product_photos_qty,
-    p.product_weight_g,
-    p.product_length_cm,
-    p.product_height_cm,
-    p.product_width_cm
+    p.product_name_lenght::int AS product_name_length,
+    p.product_description_lenght::int AS product_description_length,
+    p.product_photos_qty::int AS product_photos_qty,
+    p.product_weight_g::numeric AS product_weight_g,
+    p.product_length_cm::numeric AS product_length_cm,
+    p.product_height_cm::numeric AS product_height_cm,
+    p.product_width_cm::numeric AS product_width_cm
 
 FROM {{ source('raw', 'products') }} p
 LEFT JOIN {{ source('raw', 'product_category_translation') }} t
